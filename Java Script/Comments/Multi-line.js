ab759ced2e4f17fdcd0d6f7025fca605eb131776
Raju Singh <rajusingh.mca26@gmail.com>
2025-09-29 23:15:08 +0530

    Comments Added in JS

---
+++ Java Script/Comments/Multi-line.js
@@ -0,0 +1,7 @@
+/* 
+This is a multi-line comment.
+You can write as many lines as you want.
+It is ignored by JavaScript.
+*/
+let y = 20;
+console.log(y); // 20
---
+++ Java Script/Comments/Single-line.js
@@ -0,0 +1,4 @@
+
+// This is a single-line comment
+let x = 10; //  we can also add a comment after code
+console.log(x); // Prints 10
